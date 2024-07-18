dcaf60187273502bd597de7b17cdee22ab495481
FierensDenis <fierens.dev@gmail.com>
2024-07-18 02:49:42 +0200

    add figma file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
 
+type CardProps = {
+  str: string
+}
+
+const Card = ({ str }: CardProps) => {
+  return (
+    <div>
+      <h1>{str}</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <h1>PORTFOLIO</h1>
+
+      <Card str='salut' />
     </div>
   );
 }
