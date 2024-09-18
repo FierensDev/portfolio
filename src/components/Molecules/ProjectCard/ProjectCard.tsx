379b9aed254a33081b33794fee5aea1aff405842
FierensDenis <fierens.dev@gmail.com>
2024-09-18 12:48:58 +0200

    add idla card animation

--- src/components/Molecules/ProjectCard/ProjectCard.tsx
+++ src/components/Molecules/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { Separator } from "../../Atoms/Separator/Separator"
 import './ProjectCard.css'
+import ArrowSvg from '../../../assets/arrow.svg'
 
 interface ProjectCardProps{
   icon: string,
@@ -29,6 +30,35 @@ export function ProjectCard({icon, iconAlt, img, imgAlt, title1, title2, subtitl
       <div className='projet__item__title idla' style={{color:color}}> {title1} <br /> {title2} </div>
       <div className='projet__item__description'><span className='projet__item__description idla'  style={{color:color}}>{subtitle1}</span> {subtitle2}</div>
 {/* var Onhover ===true ?  */}
+      {title1 === "Investir dans" ? 
+         <div className="projet__item__idla">
+          <div className="">
+            <div className="projet__item__idla__separator-idla adapt-to-container">
+              <div className="projet__item__idla__separator__title-idla">année</div>
+              <div className="projet__item__idla__separator__line-idla"></div>
+            </div>
+           <p className='color-idla text-20'>2022 . 2024</p>
+          </div>
+          {/* <div className="color-white text-20"> 
+            <p>Réalisation de plusieurs projets en solo ou en équipe, avec une spécialisation en web afin de me perfectionner en front-end et back-end tout en intégrant différentes technologies.</p>
+          </div> */}
+          <Link to="/portfolio/idla" className='contact__button text-h3 color-white hover-color-idla place-right ' id="section_contact">
+            <span className='text-h3 color-white hover-color-idla'>En savoir plus</span>
+            <div className='arrow rotate-45'>
+              <svg width="100%" height="100%" viewBox="0 0 13 13" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.9016 1.09836L11.8751 0.0987085L12.9291 0.0707975L12.9012 1.12483L11.9016 1.09836ZM1.8054 12.6087C1.41488 12.9993 0.781715 12.9993 0.39119 12.6087C0.000666076 12.2182 0.000665739 11.5851 0.39119 11.1945L1.8054 12.6087ZM3.45544 0.321664L11.8751 0.0987085L11.9281 2.09801L3.50838 2.32096L3.45544 0.321664ZM12.9012 1.12483L12.6783 9.5445L10.679 9.49156L10.9019 1.07189L12.9012 1.12483ZM12.6087 1.80546L1.8054 12.6087L0.39119 11.1945L11.1945 0.391251L12.6087 1.80546Z" fill="currentColor"/>
+              </svg>
+            </div>
+          </Link>
+         {/* <div>
+           php, javascript, Typescript, java, python, GO, html, css, SQL
+           React, Vu, Angular, Next, Node, Symfony, Laravel, Ruby, Springboot, react native
+         </div> */}
+       </div>
+      :
+        <></>
+      }
+
       {title1 === "Epitech" ? 
         <div className="epitech">
           <div>
@@ -57,11 +87,10 @@ export function ProjectCard({icon, iconAlt, img, imgAlt, title1, title2, subtitl
               <div className="separator__title-esgi">année</div>
               <div className="separator__line-esgi"></div>
             </div>
-            <p className='color-esgi text-20'>2022 . 2024</p>
+            <p className='color-esgi text-20'>2024 . 2027</p>
           </div>
-          <div>
-            php, javascript, Typescript, java, python, GO, html, css, SQL
-            React, Vu, Angular, Next, Node, Symfony, Laravel, Ruby, Springboot, react native
+          <div className="color-white text-20"> 
+          Face à une constante évolution du secteur du Web, tant au niveau des langages de programmation utilisés que des méthodes de gestion de projet, la filière Ingénierie du Web nous forme à devenir expert en développement web Full-Stack.
           </div>
         </div>
       :
