b567798732178b933a849c67dd303ba602644343
FierensDenis <fierens.dev@gmail.com>
2024-09-21 20:18:10 +0200

    slider

--- src/components/Organisms/SliderTech/SliderTech.tsx
+++ src/components/Organisms/SliderTech/SliderTech.tsx
@@ -2,6 +2,27 @@ import './SliderTech.css'
 import { SliderTechCard } from "../../Molecules/SliderTechCard/SliderTechCard";
 import PostgreSQLSVG from '../../../assets/postgresql.svg'
 import NextSVG from '../../../assets/next.svg'
+import HtmlSvg from '../../../assets/langages/html-5.svg'
+import CssSvg from '../../../assets/langages/css-3.svg'
+import JavascriptSvg from '../../../assets/langages/javascript.svg'
+import PhpSvg from '../../../assets/langages/php.svg'
+import JavaSvg from '../../../assets/langages/java.svg'
+import NodeSvg from '../../../assets/langages/node-js.svg'
+import GitSvg from '../../../assets/langages/git.svg'
+import TypescriptSvg from '../../../assets/langages/typescript.svg'
+import MySqlSvg from '../../../assets/langages/mysql.svg'
+import MongoDbSvg from '../../../assets/langages/mongodb.svg'
+import BootstrapSvg from '../../../assets/langages/bootstrap.svg'
+import TailwindSvg from '../../../assets/langages/tailwind.svg'
+import AngularSvg from '../../../assets/langages/angular.svg'
+import VueSvg from '../../../assets/langages/vuejs.svg'
+import ReactSvg from '../../../assets/langages/react.svg'
+import ExpressSvg from '../../../assets/langages/express.svg'
+import JquerySvg from '../../../assets/langages/jquery.svg'
+import LaravelSvg from '../../../assets/langages/laravel.svg'
+import SymfonySvg from '../../../assets/langages/symfony.svg'
+import SpringbootSvg from '../../../assets/langages/spring-boot.svg'
+
 
 export function SliderTech(){
   return(
@@ -9,16 +30,59 @@ export function SliderTech(){
       <div className='slider-ToRight'>
         <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
         <SliderTechCard title={'NextJS'} img={NextSVG} imgAlt='Next'/>
-        <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
-        <SliderTechCard title={'NextJS'} img={NextSVG} imgAlt='Next'/>
-        <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
-        <SliderTechCard title={'NextJS'} img={NextSVG} imgAlt='Next'/>
-        <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
-        <SliderTechCard title={'NextJS'} img={NextSVG} imgAlt='Next'/>
-        <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
+        <SliderTechCard title={'HTML'} img={HtmlSvg} imgAlt='HTML-5'/>
+        <SliderTechCard title={'CSS'} img={CssSvg} imgAlt='CSS'/>
+        <SliderTechCard title={'Javascript'} img={JavascriptSvg} imgAlt='Javascript'/>
+        
+        <SliderTechCard title={'PHP'} img={PhpSvg} imgAlt='PHP'/>
+        <SliderTechCard title={'Java'} img={JavaSvg} imgAlt='Java'/>
+        <SliderTechCard title={'NodeJS'} img={NodeSvg} imgAlt='NodeJS'/>
+        <SliderTechCard title={'Git'} img={GitSvg} imgAlt='Git'/>
+        <SliderTechCard title={'Typescript'} img={TypescriptSvg} imgAlt='Typescript'/>
+        
+        <SliderTechCard title={'MySQL'} img={MySqlSvg} imgAlt='MySQL'/>
+        <SliderTechCard title={'MongoDB'} img={MongoDbSvg} imgAlt='MongoDB'/>
+        <SliderTechCard title={'Bootstrap'} img={BootstrapSvg} imgAlt='Bootstrap'/>
+        <SliderTechCard title={'Tailwind'} img={TailwindSvg} imgAlt='Tailwind'/>
+        <SliderTechCard title={'Angular'} img={AngularSvg} imgAlt='Angular'/>
+        
+        <SliderTechCard title={'ReactJS'} img={ReactSvg} imgAlt='ReactJS'/>
+        <SliderTechCard title={'ExpressJS'} img={ExpressSvg} imgAlt='ExpressJS'/>
+        <SliderTechCard title={'Jquery'} img={JquerySvg} imgAlt='Jquery'/>
+        <SliderTechCard title={'Laravel'} img={LaravelSvg} imgAlt='Laravel'/>
+        <SliderTechCard title={'Symfony'} img={SymfonySvg} imgAlt='Symfony'/>
+        
+        <SliderTechCard title={'Spring Boot'} img={SpringbootSvg} imgAlt='Spring Boot'/>
+        <SliderTechCard title={'VueJS'} img={VueSvg} imgAlt='VueJS'/>
 
-{/* 9more */}
 
+
+        <SliderTechCard title={'PostgreSQL'} img={PostgreSQLSVG} imgAlt='postgreSQL'/>
+        <SliderTechCard title={'NextJS'} img={NextSVG} imgAlt='Next'/>
+        <SliderTechCard title={'HTML'} img={HtmlSvg} imgAlt='HTML-5'/>
+        <SliderTechCard title={'CSS'} img={CssSvg} imgAlt='CSS'/>
+        <SliderTechCard title={'Javascript'} img={JavascriptSvg} imgAlt='Javascript'/>
+        
+        <SliderTechCard title={'PHP'} img={PhpSvg} imgAlt='PHP'/>
+        <SliderTechCard title={'Java'} img={JavaSvg} imgAlt='Java'/>
+        <SliderTechCard title={'NodeJS'} img={NodeSvg} imgAlt='NodeJS'/>
+        <SliderTechCard title={'Git'} img={GitSvg} imgAlt='Git'/>
+        <SliderTechCard title={'Typescript'} img={TypescriptSvg} imgAlt='Typescript'/>
+        
+        <SliderTechCard title={'MySQL'} img={MySqlSvg} imgAlt='MySQL'/>
+        <SliderTechCard title={'MongoDB'} img={MongoDbSvg} imgAlt='MongoDB'/>
+        <SliderTechCard title={'Bootstrap'} img={BootstrapSvg} imgAlt='Bootstrap'/>
+        <SliderTechCard title={'Tailwind'} img={TailwindSvg} imgAlt='Tailwind'/>
+        <SliderTechCard title={'Angular'} img={AngularSvg} imgAlt='Angular'/>
+        
+        <SliderTechCard title={'ReactJS'} img={ReactSvg} imgAlt='ReactJS'/>
+        <SliderTechCard title={'ExpressJS'} img={ExpressSvg} imgAlt='ExpressJS'/>
+        <SliderTechCard title={'Jquery'} img={JquerySvg} imgAlt='Jquery'/>
+        <SliderTechCard title={'Laravel'} img={LaravelSvg} imgAlt='Laravel'/>
+        <SliderTechCard title={'Symfony'} img={SymfonySvg} imgAlt='Symfony'/>
+        
+        <SliderTechCard title={'Spring Boot'} img={SpringbootSvg} imgAlt='Spring Boot'/>
+        <SliderTechCard title={'VueJS'} img={VueSvg} imgAlt='VueJS'/>
       </div>
     </div>
   )
